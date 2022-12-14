d6ce1b0c2357f05d3a59a2e13be1a6f6ad6ec3ce
ZMG <zaidoune1221@gmail.com>
2022-12-14 00:21:31 +0000

    Fixed chosenTimeButton

---
+++ buttons.js
@@ -0,0 +1,73 @@
+//Importing All Classes from the classes file
+import {Category, Player, Inputs} from './classes.js';
+//Importing all Functions from the functions file
+import {countdownTimerA, chooseLetter,hydrateData} from './functions.js'
+
+
+  //Storing all ElementIDs
+  const keyLetterElement = document.getElementById("keyLetter");
+  const playButton = document.getElementById("playButton");
+  const chosenTimButton = document.getElementById('chosenTimButton')
+
+
+// Event Listner for when user wants to change length of rounds
+chosenTimButton.addEventListener('click',function() {
+    let chosenTimer = prompt('Choose a Timer ');
+  
+    while (true) {
+      // Check if chosenTimer is a number
+      let isNumber = /^\d+$/.test(chosenTimer);
+  
+      // If chosenTimer is not a number, prompt the user again
+      if (!isNumber) {
+        chosenTimer = prompt("Time can only be set using intergers. Please enter a valid number:");
+        continue;
+      }
+  
+      // Parse chosenTimer to an integer
+      let number = parseInt(chosenTimer);
+  
+      // If the number is greater than 300, prompt the user again
+      if (number > 300) {
+        chosenTimer = prompt("Time can only be less than 300s. Please enter a valid number:");
+        continue;
+      }
+      
+      // If the number is valid, break the loop
+      break;
+  }
+  
+    window.chosenTimer = chosenTimer; 
+    console.log('Timer has now been changed to '+ chosenTimer + 's')
+  });
+
+
+
+
+
+  //The Game will Only start when the button is clicked
+let clicked = false
+
+playButton.addEventListener("click", function() {
+  if (clicked) {
+    // Code to execute when the button is clicked again - to restart game
+    console.log("Button clicked again! Game should end here");
+  } else {
+    // Code to execute when the button is clicked for the first time
+      // Choose a letter
+    const keyLetter = chooseLetter();
+    //Start Countdown 
+    if (typeof chosenTimer !== "undefined") {
+      countdownTimerA(chosenTimer);
+    } else{
+      countdownTimerA()
+    }
+    clicked = true;
+
+  if (keyLetterElement) {
+    keyLetterElement.innerHTML = keyLetter;
+  }
+  }});
+
+
+
--- index.js
+++ index.js
@@ -5,65 +5,15 @@ import {countdownTimerA, chooseLetter,hydrateData} from './functions.js'
 
 
 
-//Storing all ElementIDs
-const keyLetterElement = document.getElementById("keyLetter");
-const playButton = document.getElementById("playButton");
-const chosenTimButton = document.getElementById('chosenTimButton')
 
 
-chosenTimButton.addEventListener('click',function() {
-  let chosenTimer = prompt('Choose a Timer ');
-  //Continously ask the user until their input is a number
-  let isNumber = /^\d+$/.test(chosenTimer);
-  while (!isNumber & number) {
-    chosenTimer = prompt("Time can only be set using intergers. Please enter a valid number:");
-    isNumber = /^\d+$/.test(chosenTimer);
-    let number = parseInt(chosenTimer);
-    if (number > 300){ ///something to fix here
-      chosenTimer = prompt("Time can only be less than 300s . Please enter a valid number:");
-    } else{
-      let number = false 
-    }
-  }
   
-//   let number = parseInt(chosenTimer);
-
-// // Check if the number is less than 1000
-//   while (number > 1000) {
-//     chosenTimer = prompt("Time can only be less than 300s . Please enter a valid number:");
-//     isNumber = /^\d+$/.test(chosenTimer);
-// } ;
 
 
-  window.chosenTimer = chosenTimer; 
-  console.log('Timer has now been changed to '+ chosenTimer + 's')
-});
-  
+
+
+
 let Zaid = new Player("Zaid", 50, ["Salmon", "Football"]);
 console.log(Zaid.name);
 
-
-//The Game will Only start when the button is clicked
-let clicked = false
-
-playButton.addEventListener("click", function() {
-  if (clicked) {
-    // Code to execute when the button is clicked again - to restart game
-    console.log("Button clicked again! Game should end here");
-  } else {
-    // Code to execute when the button is clicked for the first time
-      // Choose a letter
-    const keyLetter = chooseLetter();
-    //Start Countdown 
-    if (typeof chosenTimer !== "undefined") {
-      countdownTimerA(chosenTimer);
-    } else{
-      countdownTimerA()
-    }
-    clicked = true;
-
-  if (keyLetterElement) {
-    keyLetterElement.innerHTML = keyLetter;
-  }
-  }});
 
