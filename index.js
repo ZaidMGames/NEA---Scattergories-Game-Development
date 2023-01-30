4cb6f3841471bf9bd7ede503dd1fefbb288f4ed8
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-01-30 11:21:22 +0000

    .

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 import {Category, Player, Inputs} from './classes.js';
 //Importing all Functions from the functions file
 import {countdownTimerA, chooseLetter,hydrateData} from './functions.js'
-// importing all buttons
-import {} from './buttons.js'
+
 
 
 
