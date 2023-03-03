62e1ad0b99c1cd7cee212b16883a0b6ee4b5d634
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-03-03 12:20:19 +0000

    asefasdfasdf

--- buttons.js
+++ buttons.js
@@ -150,7 +150,7 @@ playButton.addEventListener("click", function() {
     }
   }
     createInputBoxes();
-    console.log(typeOf(keyLetter) + console.log(keyLetter))
+    console.log(typeof(keyLetter) + console.log(keyLetter))
     // Start the countdown timer
     stopTimer.value = false;
     console.log('The Variable type of the timer is: ' + typeof chosenTimer)
