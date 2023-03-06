6d7b05173393f37c1abc542225c6579b471d792f
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-03-06 01:54:38 +0000

    asfdfsaddsf

--- functions.js
+++ functions.js
@@ -1,31 +1,40 @@
 import {Category, Player, Inputs} from './classes.js';
 import {chosenNumCategoriesInput, keyLetter} from './buttons.js'
 
+const ListOfCategories = fetch('categories.txt')
+  .then(response => response.text())
+  .then(text => {
+    return text.split('\n').map(category => new Category(category.trim()));
+  });
 
-export const ListOfCategories = [
-  new Category("Football Team"),
-  new Category("Anime"),
-  new Category("Capital City"),
-  new Category("Animal"),
-  new Category("Cartoon Character"),
-  new Category("Four Letter Word"),
-  new Category("Brand"),
-  new Category("Things on a beach"),
-  new Category("Website"),
-  new Category("Fruit"),
-  new Category("Pet Peeve"),
-  new Category("Ice Cream Flavor"),
-  new Category("Job Tittle"),
-  new Category("Language"),
-  new Category("Celebrity name"),
-  new Category("Fear"),
-  new Category("Things in a classroom"),
-  new Category("Song title"),
-  new Category("Item within eyesight"),
-  new Category("A Common Lie"),
-  new Category("Video Game"),
+export { ListOfCategories };
+
+
+
+// export const ListOfCategories = [
+//   new Category("Football Team"),
+//   new Category("Anime"),
+//   new Category("Capital City"),
+//   new Category("Animal"),
+//   new Category("Cartoon Character"),
+//   new Category("Four Letter Word"),
+//   new Category("Brand"),
+//   new Category("Things on a beach"),
+//   new Category("Website"),
+//   new Category("Fruit"),
+//   new Category("Pet Peeve"),
+//   new Category("Ice Cream Flavor"),
+//   new Category("Job Tittle"),
+//   new Category("Language"),
+//   new Category("Celebrity name"),
+//   new Category("Fear"),
+//   new Category("Things in a classroom"),
+//   new Category("Song title"),
+//   new Category("Item within eyesight"),
+//   new Category("A Common Lie"),
+//   new Category("Video Game"),
   
-]
+// ]
 
 /**
  * Shuffles an array of categories and returns a subset of a specified size.
