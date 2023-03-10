fc791c23a2160ed3f36a54adeb9f30bd1f3d6e4c
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-03-10 14:55:49 +0000

    sadsdaf

--- buttons.js
+++ buttons.js
@@ -169,17 +169,6 @@ const alertB = (message,type) => {
       alertContainer.appendChild(alertElement);
 }
 
-const alert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
 
 
 
