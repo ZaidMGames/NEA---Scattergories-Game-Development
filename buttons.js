46256caa494963c61c7f9dc423d3823a34378100
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-03-11 22:57:43 +0000

    asdfasdf

--- buttons.js
+++ buttons.js
@@ -1,7 +1,7 @@
 //Importing All Classes from the classes file
 import {Category, Player, Inputs} from './classes.js';
 //Importing all Functions from the functions file
-import {countdownTimerA, chooseLetter,hydrateData,stopTimer,displayChosenCategories,CategorySelector,ListOfCategories,createInputBoxes,getAnswers,alert} from './functions.js'
+import {countdownTimerA, chooseLetter,hydrateData,stopTimer,displayChosenCategories,CategorySelector,ListOfCategories,createInputBoxes,getAnswers,alert,displayRoundInputs} from './functions.js'
 
 let Zaid = new Player("Zaid")
 let players = [Zaid]
