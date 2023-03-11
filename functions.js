6348f859fe23f4cda8b3c41d621e02690fc61518
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-03-11 01:00:34 +0000

    sadfasdasdf

--- functions.js
+++ functions.js
@@ -196,25 +196,33 @@ function displayRoundInputs() {
   // Clear any existing content in the container
   container.innerHTML = '';
 
-  // Create a heading for the cards
+  // Create a heading for the cards with Bootstrap styling
+  const headingContainer = document.createElement('div');
+  headingContainer.classList.add('container', 'mb-5');
+  container.appendChild(headingContainer);
+
+  const headingCard = document.createElement('div');
+  headingCard.classList.add('card', 'bg-white', 'rounded', 'shadow-sm', 'p-3');
+  headingContainer.appendChild(headingCard);
+
   const heading = document.createElement('h2');
-  heading.classList.add('fw-bold', 'text-center', 'mb-5');
+  heading.classList.add('fw-bold', 'text-center', 'mb-0');
   heading.textContent = 'Here are your answers to the last couple rounds';
-  container.appendChild(heading);
+  headingCard.appendChild(heading);
 
   // Loop through each round in the dictionary
   for (const roundNumber in playerDictionary) {
     const roundInputsArray = playerDictionary[roundNumber];
 
-    // Create a Bootstrap card for the round
-    const card = document.createElement('div');
-    card.classList.add('card', 'mb-3');
-
-    // Add the card header with the round number
-    const header = document.createElement('div');
-    header.classList.add('card-header');
-    header.textContent = `Round ${roundNumber}`;
-    card.appendChild(header);
+     // Create a Bootstrap card for the round
+     const card = document.createElement('div');
+     card.classList.add('card', 'mb-3', 'border-0', 'rounded', 'shadow-sm');
+ 
+     // Add the card header with the round number
+     const header = document.createElement('div');
+     header.classList.add('card-header', 'bg-white', 'rounded', 'shadow-sm', 'fw-bold', 'border-0', 'd-flex', 'justify-content-between', 'align-items-center');
+     header.innerHTML = `<span>Round ${roundNumber}</span><i class="bi bi-chevron-down"></i>`;
+     card.appendChild(header);
 
     // Create a list group for the inputs in the round
     const listGroup = document.createElement('ul');
