e9eb7f2cad41f33df6f7031d14c7ab68219d5c35
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-03-13 00:09:30 +0000

    ...

--- functions.js
+++ functions.js
@@ -295,17 +295,15 @@ function displayMessages() {
     messageDiv.className = "message-box"; // add class for styling
     messageDiv.textContent = message;
     chatMessages.appendChild(messageDiv);
-
-    // Generate and append a replydiv
-    const reply = replies[Math.floor(Math.random() * replies.length)];
-    const replyDiv = document.createElement("div");
-    replyDiv.className = "message-box reply-box"; // add class for styling
-    replyDiv.textContent = reply;
-    chatMessages.appendChild(replyDiv);
-    // Scroll to bottom of chat-messages div
-    chatMessages.scrollTop = chatMessages.scrollHeight;
   }
-  
+  // Generate and append a replydiv
+  const reply = replies[Math.floor(Math.random() * replies.length)];
+  const replyDiv = document.createElement("div");
+  replyDiv.className = "message-box reply-box"; // add class for styling
+  replyDiv.textContent = reply;
+  chatMessages.appendChild(replyDiv);
+  // Scroll to bottom of chat-messages div
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
 
