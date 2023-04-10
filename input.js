f1d332c54eee769189eb9f9e48c06e658769fe15
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-04-10 07:33:07 +0100

    typo

--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 import {Category, Player, Inputs} from './classes.js';
 import {chosenNumCategoriesInput, keyLetter} from './buttons.js'
-import {validInput} from '.functions.js'
+import {validInput} from './functions.js'
 
 let playerDictionary = {}
 let chosenRoundCategoriesDic = {}
