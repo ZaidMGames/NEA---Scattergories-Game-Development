faaaddfe9e98a6ee988d43ca048c6c7691f7024d
ZMG <91030969+ZaidMGames@users.noreply.github.com>
2023-04-10 07:32:07 +0100

    just fixing me pasting twice

--- category.js
+++ category.js
@@ -51,35 +51,6 @@ export function displayChosenCategories(categoriesArray) {
 }
 
 
-  //Hydrate data algorithm
-export function hydrateData(players, categories) {
-    for (let i = 0; i < categories.length; i++) {
-      const category = categories[i]
-      for (const player of players) {
-        const answer = player.answers[i]
-        const input = new Inputs(player, answer, category.name)
-        category.answers[player.name] = input
-      }
-  
-      // filter out duplicates - REMOVE BOTH DUPLICATES
-      let entries = Object.entries(category.answers)
-          .map(([key, value]) => {
-            return ([key, value.answer])
-          }) // make 2D array of answers from { playername: answer (string) }
-  
-      // loop through key and value of new 2D array
-      for (let i = 0; i < entries.length; i++) {
-        const [key, value] = entries[i]
-        // if index of loop != FIRST index of occurence in list,
-        // must be a duplicate, use filter to remove all
-        if (i != entries.map(([k,v]) => v).indexOf(value)) {
-           entries = entries.filter(([k,v]) => v != value) 
-        }
-      }
-  
-      category.answers = Object.fromEntries(entries)
-    }
-  }
 
 //Hydrate data algorithm
 export function hydrateData(players, categories) {
